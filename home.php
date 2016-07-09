075a9bf469a98a8bf801d7d23855aa216f283a0b
deetriplett <deetriplett@gmail.com>
2016-07-09 13:03:34 -0700

    Changed app.txt to app.log

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 $log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
 $log->addWarning('Foo');
 
 echo "EAT ME Sugar Scrubs";
