4faba021e4094584f5c21beb4820f96d32260e96
deetriplett <deetriplett@gmail.com>
2016-07-09 13:02:43 -0700

    Created $log

--- home.php
+++ home.php
@@ -1,6 +1,10 @@
 <?php
 
-require "vendor/autoload.php";
+require __DIR__ . '/vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+$log->addWarning('Foo');
 
 echo "EAT ME Sugar Scrubs";
 
