58c13abed2220a260a0e39d6063df0e2c2c41653
deetriplett <deetriplett@gmail.com>
2016-07-09 13:19:01 -0700

    Add Correct timestamp for log

--- home.php
+++ home.php
@@ -1,9 +1,12 @@
 <?php
-
 require __DIR__ . '/vendor/autoload.php';
+date_default_timezone_set('America/Kentucky/Louisville');
 
-$log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('app.txt', Monolog\Logger::WARNING));
 $log->addWarning('Foo');
 
 echo "EAT ME Sugar Scrubs";
